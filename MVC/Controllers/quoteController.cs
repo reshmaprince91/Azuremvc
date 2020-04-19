62369822457fa95f60db380951fefbafec6c6000
Reshma Prince <reshmaprince91@gmail.com>
2020-04-19 00:34:20 -0400

    import project

---
+++ MVC/Controllers/quoteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class quoteController : Controller
+    {
+        // GET: quote
+        public ActionResult Index()
+        {
+
+            IEnumerable<quoteModel> qtemdl;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Quote").Result;
+            qtemdl = response.Content.ReadAsAsync<IEnumerable<quoteModel>>().Result;
+            return View(qtemdl);
+        }
+    }
+}
---
+++ MVC/Models/quoteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class quoteModel
+    {
+        public int quoteid { get; set; }
+        public string quotetype { get; set; }
+        public string contact { get; set; }
+        public string description { get; set; }
+        public string duedate { get; set; }
+        public string tasktype { get; set; }
+    }
+}
